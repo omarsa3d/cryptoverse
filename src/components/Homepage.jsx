d3d6a5b06d551898bf56609ba398bc352a6a20eb
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-11-30 19:13:21 +0200

    main homepage section

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+
+const { Title } = Typography
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}> <Statistic value='5' title='Total Cryptocurrencies'/> </Col>
+        <Col span={12}> <Statistic value='5' title='Total Exchanges'/> </Col>
+        <Col span={12}> <Statistic value='5' title='Total Market Cap'/> </Col>
+        <Col span={12}> <Statistic value='5' title='Total 24h Volume'/> </Col>
+        <Col span={12}> <Statistic value='5' title='Total Markets'/> </Col>
+      </Row>
+    </>
   )
 }
 
