91e804a53ed2a205742dfd6497e4aadc8764d86e
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-11-30 19:05:25 +0200

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components/index'
+import './App.css'
 
 const App = () => {
   return (
 
     <div className="app">
+
+      {/* NAVBAR SECTION */}
       <div className="navbar">
         <Navbar />
       </div>
+
+      {/* MAIN SECTION */}
       <div className="main">
         <Layout>
           <div className="routes">
@@ -22,9 +27,19 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className="footer">
 
+        {/* FOOTER SECTION */}
+        <div className="footer">
+          <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
+            Cryptoverse <br />
+            All Rights Reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   )
