d9b60e0e05630882f362e85a42dabf0479ba73b1
omarsa3d <87918992+omarsa3d@users.noreply.github.com>
2024-12-05 20:39:46 +0200

    news section added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,14 +4,16 @@ import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoApi'
 import { Cryptocurrencies, News } from '../components'
+import Loader from './Loader';
+
 const { Title } = Typography
+
 
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
-  if (isFetching) {
-    return "Loading..";
-  }
+  if (isFetching) return <Loader />;
+
 
   return (
     <>
